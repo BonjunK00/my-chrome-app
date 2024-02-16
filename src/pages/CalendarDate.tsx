a5045c552e540c1e7fd1eb9b555d7a5b76a12f22
BonjunK00 <122672353+BonjunK00@users.noreply.github.com>
2024-02-16 13:39:59 +0900

    ellipsis 처리

--- src/pages/CalendarDate.tsx
+++ src/pages/CalendarDate.tsx
@@ -27,13 +27,21 @@ export const CalendarDate = ({ dateObject, isSelected = false, isOtherMonth = fa
     <>
       <div
         className={cn(
-          'flex flex-1 hover:bg-[#E3E9F6] justify-center text-[16px] p-[1px] font-medium cursor-pointer',
+          'flex flex-col flex-1 hover:bg-[#E3E9F6] items-start text-[16px] p-[1px] font-medium cursor-pointer overflow-x-hidden',
           isOtherMonth ? 'text-[#C5CAD3]' : 'text-black',
         )}
         aria-selected={isSelected}
         onClick={handleClickDate}
       >
-        <div>{dateObject.date}</div>
+        <div className="flex justify-center w-full">{dateObject.date}</div>
+        {sortedSchedules.map((schedule) => {
+          return (
+            <div className="flex space-x-[2px] p-[2px] overflow-x-hidden w-full">
+              <div className="bg-blue-300 w-[6px] h-[18px] rounded-[3px]" />
+              <div className="text-[12px] whitespace-nowrap overflow-hidden text-ellipsis">{schedule.schedule}</div>
+            </div>
+          )
+        })}
       </div>
       {isOpen && (
         <ScheduleModal
